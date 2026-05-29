9b2f8d063bdb93b86b4c0a5e1b96958576772f93
Abhay Pandey <pandeyabhay055@gmail.com>
2026-05-29 16:27:26 +0530

    Basic execution

---
+++ src/Basics.java
@@ -0,0 +1,12 @@
+
+
+void main() {
+    System.out.println("Java Begins");
+    System.out.print("And");
+    System.out.println("DSA too");
+
+   // Hope you are enjoying
+
+    System.out.println("4+3");
+    System.out.println(7+3);
+}
