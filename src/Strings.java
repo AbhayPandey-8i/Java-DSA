abfa1580ccfe759be8ad7be8c2eb3c005563d96c
Abhay Pandey <pandeyabhay055@gmail.com>
2026-06-10 17:08:36 +0530

    Strings intro

---
+++ src/Strings.java
@@ -0,0 +1,104 @@
+import java.util.Scanner;
+
+public class Strings {
+    static void main() {
+
+
+
+//        String name = "Abhay Pandey";
+//        int count = 0;
+////        name = name.length();
+//        for (int i = 0; i <=name.length()-1 ; i++) {
+//            char ch = name.charAt(i);
+//            if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u' || ch == 'A' || ch == 'E' || ch == 'I' ||  ch == 'O' || ch == 'U' ){
+//              count++;
+//            }
+//        }
+
+
+
+//        String name = "Abhay";
+//       int n = name.length();
+//        for (int i = 0; i <=n-1 ; i++) {
+//            char ch = name.charAt(i); //Imp
+//            System.out.println(ch);
+//        }
+//
+//        String input = "Abhay, Chola, Butla, Gundi, Kaalu";
+//        String [] words = input.split(",");
+//        for(String str: words){
+//            System.out.println(str);
+//        }
+
+//        String name = "Shashikant and Chola";
+//        System.out.println(name.startsWith("S"));
+//        System.out.println(name.endsWith("a"));
+
+//        String name = "Abhay and Lyzer";
+//        System.out.println(name.toUpperCase());
+//        System.out.println(name.toLowerCase());
+//        System.out.println(name.substring(0,6));
+//        System.out.println(name.contains("Megha"));
+
+
+//        String Name = "   Abhay   ";
+//        System.out.println(Name.length());
+//        Name = Name.trim();
+//        System.out.println(Name);
+//        System.out.println(Name.length());
+
+//        String str = "  ";
+//        System.out.println(str.length());
+//        System.out.println(str.isBlank());
+//        System.out.println(str.isEmpty());
+
+
+
+//        Scanner sc = new Scanner(System.in);
+//        System.out.println("Provide string value");
+//        String str = sc.nextLine();
+//        System.out.println("value of nextLine is:" + str);
+//        String name = sc.next();
+//        System.out.println("value of next is:" + name);
+
+
+
+//        String name1 = "PANDEY";
+//        String name2 = "Pandey";
+//
+//        if (name1.equalsIgnoreCase(name2)){
+//            System.out.println("Both strings are equal");
+//        } else {
+//            System.out.println("Strings are not equal");
+//        }
+
+//        String name1 = "Pandey";
+//        String name2 = "Pandey";
+//
+//        if (name1.equals(name2)){
+//            System.out.println("Both strings are equal");
+//        } else {
+//            System.out.println("Strings are not equal");
+//        }
+
+
+//        }    String name1 = "Pandey";
+//        String name2 = "Pandey";
+//
+//        if (name1 == name2){  //Note: this "==" is not comparing content of strings, its comparing location or address of string in heap pool
+//            System.out.println("Both strings are equal");
+//        } else {
+//            System.out.println("Strings are not equal");
+//        }
+
+//         String firstName = "Abhay";
+//         String lastName = new String("Pandey");
+//        System.out.println(firstName + lastName);
+//        System.out.println(firstName.length());
+//        System.out.println(firstName.charAt(0));
+
+//        String name = "Abhay";
+//        name = "None";
+//        System.out.println(name); //note: strings are immutable, here only reference gets changes hence we are getting "None"
+    }
+}
