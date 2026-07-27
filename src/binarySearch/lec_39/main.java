210e647aca014c5cd6b371cc75afab7c33f96383
Abhay Pandey <pandeyabhay055@gmail.com>
2026-07-28 00:00:07 +0530

    pivot index

---
+++ src/binarySearch/lec_39/main.java
@@ -0,0 +1,36 @@
+package binarySearch.lec_39;
+
+public class main {
+
+    //finding pivot index
+    public int findPivotIndex(int arr[]){
+        int n = arr.length;
+        int s = 0;
+        int e = n-1;
+        int ans = -1;
+
+        if (arr[s] <= arr[e]){
+            return -1;
+        }
+
+        while (s<=e){
+            int mid =  s + (e-s)/2;
+
+            if (arr[mid] < arr[n-1]){
+                e = mid - 1;
+            } else {
+                 ans = mid;
+                s = mid + 1;
+            }
+        }
+        return ans;
+    }
+
+
+
+
+    static void main() {
+
+    }
+
+}
